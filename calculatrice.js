988c4a3eaba1ed6a5aca73412c6625d04ff782f8
kwakature <berton.arthur24@gmail.com>
2025-10-24 10:54:14 +0200

    feat:reset button + fix: operator

--- calculatrice.js
+++ calculatrice.js
@@ -1,6 +1,6 @@
 let operatorButton = document.querySelectorAll(".operator")
 let display = document.querySelector("#display")
-
+let supprimer = document.querySelector(".clear")
 let number1 = "";
 let number2 = "";
 let operator = "";
@@ -9,12 +9,20 @@ function showDisplay(element){
     display.innerHTML = element
 }
 
+function supp(){
+    number1 = ""
+    number2 = ""
+    operator = ""
+    showDisplay("0")
+}
+
+supprimer.addEventListener('click' ,supp)
 
 function operatorSelector (event){
     const op = event.target.dataset.operator;
         if (number1 === "") {
             return;
-        }else if (operator !== "" && number2 === "") {
+        }else if (operator !== "") {
             return;
         }else {
             operator = op;
@@ -23,14 +31,14 @@ function operatorSelector (event){
         
     };
 
-for(let ope of operatorButton){
-    ope.addEventListener('click',operatorSelector);
+for(let opButton of operatorButton){
+    opButton.addEventListener('click',operatorSelector);
 }
 const numberButtons = document.querySelectorAll("[data-number]");
 
 function numberButton(event) {
     const value = event.target.dataset.number;
-     if (operator === "") {
+        if (operator === "") {
             number1 += value;
             showDisplay(number1)
         } else {
@@ -41,4 +49,3 @@ function numberButton(event) {
 for (buttonNumber of numberButtons) {
     buttonNumber.addEventListener('click', numberButton);
 }
-

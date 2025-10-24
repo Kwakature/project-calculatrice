fd0b0f4968e042f2f9babbab6416526bffa3d38c
kwakature <berton.arthur24@gmail.com>
2025-10-24 10:14:09 +0200

    feat:affichage

--- calculatrice.js
+++ calculatrice.js
@@ -1,9 +1,17 @@
 let operatorButton = document.querySelectorAll(".operator")
+let display = document.querySelector("#display")
+let list = '';
+
+function showDisplay(element){
+    list += element
+    display.innerHTML = list
+}
+
 
 function operatorSelector (event){
     const buttonOpe = event.target;
     const operation = buttonOpe.dataset.operator;
-
+    showDisplay(operation)
     console.log(operation)
 }
 
@@ -11,14 +19,15 @@ for(let ope of operatorButton){
     ope.addEventListener('click',operatorSelector);
 }
 const numberButtons = document.querySelectorAll("[data-number]");
-const display = document.getElementById("display");
 
 function numberButton(event) {
     const clikedButton = event.target;
     const nb = clikedButton.dataset.number;
+    showDisplay(nb)
     console.log(nb)
     
 }
 for (buttonNumber of numberButtons) {
     buttonNumber.addEventListener('click', numberButton);
 }
+
